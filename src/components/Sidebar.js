4e997f549a8e33f00752675cfe5ce4479518765f
swaff-y <kyle@swaff.id.au>
2021-02-08 13:16:13 +1100

    Design of side bar completed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,58 @@
 import React, {useEffect, useState} from 'react';
+import {Button, IconButton} from '@material-ui/core';
+import Toc from '@material-ui/icons/Toc';
+import MailOutline from '@material-ui/icons/MailOutline';
+import { SocialIcon } from 'react-social-icons';
 import "./sidebar.css";
 
 const Sidebar = (props) => {
   return(
     <>
       <div className="sidebar">
-        <img src={process.env.PUBLIC_URL + '/img/image.png'} alt="Kyle Swaffield" className="sidebar__img"/>
-        <h1>Kyle Swaffield</h1>
-        <p>kyle@swaff.id.au</p>
-        <ul>
-          <li>Introduction</li>
-          <li>About Me</li>
-          <li>Timeline</li>
-          <li>Facebook</li>
-          <li>Twitter</li>
-          <li>LinkedIn</li>
-          <li>Medium</li>
-        </ul>
+        <div className="sidebar__header">
+          <IconButton >
+            <Toc />
+          </IconButton>
+        </div>
+
+
+        <img src={process.env.PUBLIC_URL + '/img/image.png'} alt="Kyle Swaffield" className="sidebar__img" />
+
+          <h1>Kyle Swaffield</h1>
+          <Button >
+            <MailOutline />
+            <p>kyle@swaff.id.au</p>
+          </Button>
+          <ul>
+            <li>
+            <Button >
+              Introduction
+            </Button>
+            </li>
+            <li>
+            <Button >
+              About Me
+            </Button>
+            </li>
+            <li>
+            <Button >
+              Timeline
+            </Button>
+            </li>
+          </ul>
+          <ul className="sidebar__social">
+            <li>
+              <SocialIcon url="https://www.linkedin.com/in/kyle-swaffield/" />
+            </li>
+            <li>
+              <SocialIcon url="https://twitter.com/KyleSwaffield" />
+            </li>
+            <li>
+              <SocialIcon url="https://github.com/swaff-y" />
+            </li>
+
+          </ul>
+
       </div>
     </>
   )
