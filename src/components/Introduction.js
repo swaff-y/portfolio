2af368324092783342fa8919173aaeecf93dc320
swaff-y <kyle@swaff.id.au>
2021-02-10 22:23:09 +1100

    Worked on downloadin cv

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,20 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./introduction.css";
 
 const Introduction = (props) => {
+  const [slide,setSlide] = useState(true);
+
+  const handleCVClick = () => {
+    if(slide === true){
+      setSlide(false);
+      console.log("Project", slide);
+    }else if(slide === false){
+      setSlide(true);
+      console.log("CV", slide);
+    }
+  }
+
   return(
     <div className="introduction" id="introduction">
 
-      <Carousel className="introduction__carousel">
-        <Carousel.Item interval={5000} indicators={false}>
-
+      <Carousel className="introduction__carousel" controls={false} indicators={false} onSlide={handleCVClick}>
+        <Carousel.Item interval={5000}>
             <h1><strong>Hi, I'm Kyle</strong></h1>
             <p>A full stack software engineer based in Sydney</p>
             <div className="introduction__callToAction">
-              Download my Resume
+              <a href="https://swaff-y.github.io/portfolio/cv.pdf" download="w3logo">Download my Resume</a>
             </div>
-
         </Carousel.Item>
-        <Carousel.Item interval={5000} indicators={false}>
+        <Carousel.Item interval={5000}>
             <h1>I love building things</h1>
             <p>Why not check out some of my work?</p>
             <div className="introduction__callToAction">
